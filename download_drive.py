70f8db0083c707ad94bc3aaec853aa7e33ed6207
fawaaid1011 <fawaaid1011@gmail.com>
2026-08-25 10:41:00 +0700

    Update TARGET_AKUN to use numeric strings

--- download_drive.py
+++ download_drive.py
@@ -9,7 +9,7 @@ from googleapiclient.http import MediaIoBaseDownload
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 # Filter target akun yang ingin didownload saja
-TARGET_AKUN = ['akun-25', 'akun-26', 'akun-27']
+TARGET_AKUN = ['25', '26', '27']
 
 def main():
     sa_key_info = os.environ.get('GCP_SA_KEY')
